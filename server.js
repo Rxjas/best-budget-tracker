9691b830accd570d296d4e19a806230bd34eca38
Christopher Rojas <christopherrojas35@yahoo.com>
2020-12-12 15:15:41 -0700

    changes for mongoDB atlas  deployment

--- server.js
+++ server.js
@@ -15,12 +15,19 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-});
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false,
+//   useCreateIndex: true
+// });
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/todo_db', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    })
 
 // routes
 app.use(require("./routes/api.js"));
