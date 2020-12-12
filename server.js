5c2f6745d313b265f4e9b54a489394d86be378ce
Christopher Rojas <christopherrojas35@yahoo.com>
2020-12-12 15:46:09 -0700

    Forgot to make it so port can use heroku port in server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const path = require("path");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -31,8 +30,8 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
     })
 
 // routes
-app.use(require("./routes/api.js"));
 app.use(require("./routes/html.js"));
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
