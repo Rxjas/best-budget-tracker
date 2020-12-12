f77ea9a129b36599dc861b07277eca2cc89dfe69
Christopher Rojas <christopherrojas35@yahoo.com>
2020-12-12 15:40:15 -0700

    created a / route that serves the html page

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 const compression = require("compression");
 
 const PORT = 3000;
@@ -31,6 +32,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
 
 // routes
 app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
